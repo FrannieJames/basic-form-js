22b7fb5f63b7fe0a241ed86dfa3d4b6d53fcbb0a
FrannieJames <ballenasfra.ne08@gmail.com>
2020-03-17 06:51:50 +0800

    adjustment

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-let firstName = document.getElementById('firstName').value;
+let firstName = document.getElementById('firstName');
 let secondName = document.querySelector('#firstName').value;
 
 console.log(firstName);
 
 function getData(){
 
-    let input = document.querySelector('#firstName').value;
+    let input = document.querySelector('#secondName').value;
     let output = document.querySelector('#output');
     output.setAttribute('value', input);
 }
